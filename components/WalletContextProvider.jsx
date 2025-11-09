086639eeb44a51b169df22930ef83677eb869eff
New Addict <121753715+raffxv@users.noreply.github.com>
2025-11-09 09:33:56 +0700

    Update WalletContextProvider.jsx

--- components/WalletContextProvider.jsx
+++ components/WalletContextProvider.jsx
@@ -7,12 +7,12 @@ import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adap
 require('@solana/wallet-adapter-react-ui/styles.css')
 
 const WalletContextProvider = ({ children }) => {
-  const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
+  const wallets = [new PhantomWalletAdapter()];
   const endpoint = "https://cool-clean-friday.solana-mainnet.quiknode.pro/5d4f1cb01a8c07492695c52553b7b7d525926a7f/"
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets}>
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
